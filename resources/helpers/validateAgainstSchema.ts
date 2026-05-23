16d885c33f831f96d9d731d1220b9a1361c6c839
DanielObirije <obirijedaniel@gmail.com>
2026-05-23 23:22:42 +0100

    add validateschema

--- resources/helpers/validateAgainstSchema.ts
+++ resources/helpers/validateAgainstSchema.ts
@@ -1,2 +1,12 @@
 // import { addWarning } from "./warning";
 // import * as fileSystem from "fs";
+
+export async function schemaParameters(schema: string, docs: string) {
+  try {
+    const apiDocs = JSON.parse(fs.readFileSync(`./${docs}_spec3.json`).toString("utf-8"));
+
+    return apiDocs.components.schemas[schema].properties;
+  } catch (e) {
+    fail(`The '${schema}' object you passed does not exist in '${docs}' documentation`);
+  }
+}
